8a10fa54f386e9e57a5663af3c97b80cf42a6567
AnirudhBagalkotker <93025106+AnirudhBagalkotker@users.noreply.github.com>
2023-03-28 19:11:13 +0530

    Update Login page UI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,12 +12,12 @@ const Form = () => {
 	const lastName = useRef();
 
 	return (
-		<section className='p-10 h-full flex flex-col items-center justify-center'>
-			<div className="ball b1"></div>
-			<div className="ball b2"></div>
-			<img src="svg.svg" alt="" className='h-28 w-auto mt-20 mb-0' />
-			<div className='mx-auto h-full p-10 flex flex-col items-center justify-center'>
-				<h1 className='text-8xl mb-5 font-bold text-white'>Sign in</h1>
+		<section className='p-2 sm:p-10 h-full flex flex-col items-center justify-center'>
+			<div className="ball b1 w-[7	0vw] sm:w-[40vw] md:w-[30vw] lg:w-[18vw]"></div>
+			<div className="ball b2 w-[30vw] sm:w-[25vw] md:w-[20vw] lg:w-[10vw]"></div>
+			<img src="svg.svg" alt="" className='h-[12.5%] mb:h-[20%] sm:h-[25%] w-auto mt-[30vh] sm:mt-[20vh] md:mt-[15vh] mb-[10vh] sm:mb-0' />
+			<div className='mx-auto h-full p-2 md:p-10 flex flex-col justify-start text-center sm:items-center sm:ustify-center'>
+				<h1 className='text-6xl sm:text-7xl md:text-8xl mb-10 font-bold text-white '>Sign in</h1>
 				<button>
 					<img src="G.svg" alt="" className='w-5 h-auto mr-3' />
 					Continue with Google
